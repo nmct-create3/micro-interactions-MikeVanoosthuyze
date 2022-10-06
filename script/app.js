53a209d0f33d955651ab97c1b7f819a541b07974
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2022-10-06 17:40:07 +0200

    Validation nog niet gemaakt

--- script/app.js
+++ script/app.js
@@ -1,9 +1,42 @@
+
+
+
 function handleFloatingLabel() {
-   
+   const input = document.querySelector(".js-floating-input");
+   const label = document.querySelector(".js-floating-label");
+
+    input.addEventListener("focus", function() {
+        label.classList.add("is-floating");
+    });
+
+    input.addEventListener("blur", function() {
+        if (!input.value) {
+            label.classList.remove("is-floating");
+        } else {
+            label.classList.add("is-floating");
+        }
+    });
+
 }
 
 function handlePasswordSwitcher() {
-    
+
+    // const passwordOptions = ['password', 'text'];
+
+    // passwordToggle.addEventListener('change', function () {
+    //     passwordInput.type = passwordOptions[+this.checked];
+    // });
+    const btn = document.querySelector(".js-password-toggle");
+
+    btn.addEventListener("change", function() {
+        const input = document.querySelector(".js-password-input")
+        if(input.type === "password") {
+            input.type = "text";
+        } else {
+            input.type = "password";
+        }
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
